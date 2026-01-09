942109aa57fd86dabe626813a366cd935e1f5eb0
Mounika <codes.mounika@gmail.com>
2026-01-09 15:05:54 +0530

    Add files via upload

--- python_calculator.py
+++ python_calculator.py
@@ -1,18 +1,25 @@
+def main():
+    try:
+        n1 = float(input("Enter number1: "))
+        n2 = float(input("Enter number2: "))
+    except ValueError:
+        print("Invalid number entered. Please enter numeric values only.")
+        return  # Stops function safely if input is bad
+    op = input("Enter operator (+, -, *, /): ").strip()
 
-n1=float(input("Enter number1 : "))
-n2=float(input("Enter number2 : "))
-op =input("Enter the operator : ")
-if op=='+':
-    print("the result is : ",n1+n2)
-elif op=='-':
-    print("the result is : ",n1-n2)
-elif op=='*':
-    print("the result is : ",n1*n2)
-elif op=='/':
-    if n2!=0:
-        print("the result is : ",n1/n2)
+    if op == '+':
+        print("The result is:", n1 + n2)
+    elif op == '-':
+        print("The result is:", n1 - n2)
+    elif op == '*':
+        print("The result is:", n1 * n2)
+    elif op == '/':
+        if n2 != 0:
+            print("The result is:", n1 / n2)
+        else:
+            print("Error: Division by zero is not allowed.")
     else:
-        print("Error: Division by zero is invalid")
-else:
-    print(" Opertor not supported")
+        print("Operator not supported. Please use +, -, *, or /.")
 
+if __name__ == "__main__":
+    main()
